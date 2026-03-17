1e33bff29e077fdeb15b40d67f722878f6c86655
GarifullinII <ildaridrisovich@gmail.com>
2026-03-17 13:19:02 +0400

    Initial Commit

---
+++ NewsSwiftUI/NewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSwiftUIApp.swift
+//  NewsSwiftUI
+//
+//  Created by Ildar Garifullin on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct NewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
